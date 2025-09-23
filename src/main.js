5317dab83c4050a6a2a97356b5bacbf38ba56549
kmudhut <kamil.lepianka.03@gmail.com>
2025-09-24 00:21:18 +0200

    Created WeatherApp class with a private method mapping all HTML elements with an id attribute to an object. Added event listener that creates new instance of WeatherApp on DOMContentLoaded event.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+class WeatherApp {
+    constructor() {
+        this.viewElements = {}
+        this.#bindHTMLElements();
+    }
+    #bindHTMLElements(){
+        const listOfIds = [...document.querySelectorAll('[id]')].map((elem) => elem.id);
+        for(const id of listOfIds){
+            this.viewElements[id] = document.getElementById(id);
+        }
+        console.log(this.viewElements);
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', new WeatherApp());
