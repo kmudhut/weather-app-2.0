e0588eb23f458b7795284c1304be4c7fea533795
kmudhut <kamil.lepianka.03@gmail.com>
2025-11-01 01:57:14 +0100

    Created getCurrentWeatherByCoordinates function that fetches weather data from weatherapi.com. Added fadeInOut() and switchView() methods for animated view switching using Animate.css library. Created private method #setupListeners.

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,17 @@
+
+
 export const getFuzzyMatchedCityNames =  (cityname) => {
     return fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityname}&count=10`)
         .then(res=>res.json())
         .then(({results:cities}) => {
            return cities.map(({name:name, latitude:lat, longitude:lon, admin1: voivodeship})=> ({name, lat, lon, voivodeship}));
+        });
+}
+
+export const getCurrentWeatherByCoordinates =  ({lat,lon}) => {
+    return fetch(`https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_WEATHERAPI_COM_API_KEY}&q=${lat},${lon}&aqi=no`)
+        .then(res=>res.json())
+        .then((res) => {
+            console.log(res);
         });
 }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,56 @@
-import {getFuzzyMatchedCityNames} from "./apiService.js";
+import {getCurrentWeatherByCoordinates, getFuzzyMatchedCityNames} from "./apiService.js";
 
 class WeatherApp {
-    #coordinatesToSearch = {lat:null, lon:null};
+    #coordinatesToSearch = {lat: null, lon: null};
     constructor() {
         this.viewElements = {}
         this.#bindHTMLElements();
-        this.viewElements["searchInput"].addEventListener('input', this.generateInputSearchSuggestionList.bind(this))
+        this.#setupListeners()
 
     }
-   #bindHTMLElements(){
+
+    fadeInOut(parentElement, childToRemove, childToShow) {
+        const onAnimationEnd = () => {
+            parentElement.style.display = "none";
+            parentElement.classList.remove('animate__animated', 'animate__bounceOut');
+            childToRemove.style.display = "none"
+            parentElement.removeEventListener('animationend', onAnimationEnd);
+            childToShow.style.display = "flex"
+            parentElement.style.display = "initial";
+            parentElement.classList.add('animate__animated', 'animate__bounceIn');
+        }
+        parentElement.classList.add('animate__animated', 'animate__bounceOut');
+        parentElement.addEventListener('animationend', onAnimationEnd);
+    }
+
+    switchView() {
+        if (this.viewElements["weatherSearchView"].style.display !== "none") {
+            this.fadeInOut(this.viewElements["mainContainer"], this.viewElements["weatherSearchView"], this.viewElements["weatherForecastView"]);
+        } else if (this.viewElements["weatherForecastView"].style.display !== "none") {
+            this.fadeInOut(this.viewElements["mainContainer"], this.viewElements["weatherForecastView"], this.viewElements["weatherSearchView"]);
+        }
+    }
+
+    #bindHTMLElements() {
         const listOfIds = [...document.querySelectorAll('[id]')].map((elem) => elem.id);
-        for(const id of listOfIds){
+        for (const id of listOfIds) {
             this.viewElements[id] = document.getElementById(id);
         }
+    }
 
+    #setupListeners() {
+        this.viewElements["searchInput"].addEventListener('input', this.generateInputSearchSuggestionList.bind(this));
+        this.viewElements["searchInput"].addEventListener('keydown', (e) => {
+            if (e.key === "Enter") {
+                getCurrentWeatherByCoordinates(this.#coordinatesToSearch);
+                console.log("ENTER HIT");
+
+            }
+        });
+        this.viewElements["searchButton"].addEventListener('click', e => {
+            this.switchView();
+        })
+        this.viewElements["returnToSearchViewBtn"].addEventListener('click', this.switchView.bind(this))
     }
 
     async generateInputSearchSuggestionList() {
@@ -21,7 +58,7 @@ class WeatherApp {
         const matchedCityNames = await getFuzzyMatchedCityNames(inputValue);
         this.viewElements.searchInputSuggestionsList.innerHTML = "";
         console.log(matchedCityNames);
-        for(let city of matchedCityNames){
+        for (let city of matchedCityNames) {
             let li = document.createElement("li");
             let span = document.createElement("span");
             span.innerText = city?.voivodeship;
@@ -34,6 +71,7 @@ class WeatherApp {
             li.addEventListener("click", (e) => {
                 this.#coordinatesToSearch.lat = e.target?.dataset.lat;
                 this.#coordinatesToSearch.lon = e.target?.dataset.lon;
+                this.viewElements.searchInputSuggestionsList.style.display = "none";
             })
             this.viewElements?.searchInputSuggestionsList.append(li);
         }
