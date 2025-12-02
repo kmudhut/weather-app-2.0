5ba14b112b715644f6908a4e5708479617a82ca4
kmudhut <kamil.lepianka.03@gmail.com>
2025-12-02 13:20:34 +0100

    Added API errors handling.

--- src/apiService.js
+++ src/apiService.js
@@ -4,10 +4,12 @@ export const getFuzzyMatchedCityNames =  (cityname) => {
         .then(({results:cities}) => {
            return cities.map(({name:name, latitude:lat, longitude:lon, admin1: voivodeship})=> ({name, lat, lon, voivodeship}));
         })
+        .catch((err)=>{throw {error:err, type:"API_ERROR"}});
 }
 
-export const getCurrentWeatherByCoordinates =  ({lat,lon}) => {
-    return fetch(`https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_WEATHERAPI_COM_API_KEY}&q=${lat},${lon}&aqi=no`)
+export const  getCurrentWeatherByCoordinates =  ({lat,lon}) => {
+    return fetch(`https://api.weatherapi.com/v1,9/current.json?key=${import.meta.env.VITE_WEATHERAPI_COM_API_KEY}&q=${lat},${lon}&aqi=no`)
         .then(res=>res.json())
-        .then((res) => res);
+        .then((res) => res)
+        .catch((err)=>{throw {error:err, type:"API_ERROR"}});
 }
